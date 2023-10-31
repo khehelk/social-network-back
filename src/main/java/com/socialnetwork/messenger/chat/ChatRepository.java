58726013ac18bf474c40e1b89358fab8380813c6
Danil Markov <danilmarkov88@gmail.com>
2023-10-31 23:13:24 +0400

    feature: basic jpa repositories

---
+++ src/main/java/com/socialnetwork/messenger/chat/ChatRepository.java
@@ -0,0 +1,6 @@
+package com.socialnetwork.messenger.chat;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRepository extends JpaRepository<Chat, Long>{    
+}
